a0563142db57a155080e8b691a432aebe9a47ff5
lucasbarrossantos <lucas-barros28@hotmail.com>
2020-10-22 09:19:09 -0300

    Projeto Config Server

---
+++ src/test/java/br/com/alura/microservice/config/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservice.config.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
